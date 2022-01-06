9b802bbb11ea08217db294a74e981c8a68325ef2
zmtmqhdl <91964419+zmtmqhdl@users.noreply.github.com>
2022-01-06 17:15:06 +0900

    백준_10869번.py

---
+++ 백준_10869번.py
@@ -0,0 +1,6 @@
+A, B = map(int, input().split())  # map(적용시킬 함수, 적용할 값)
+print(A + B) # 덧셈은 + 사용
+print(A - B) # 뺄셈은 - 사용
+print(A * B) # 곱셈은 * 사용
+print(int(A / B)) # 나눗셈은 / 사용하며 int형을 사용해 정수로 변환
+print(A % B) # 나눗셈의 나머지는 % 사용
