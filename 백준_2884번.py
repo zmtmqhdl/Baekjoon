9d9961f3a85e9df74831920d519455211e08d013
zmtmqhdl <91964419+zmtmqhdl@users.noreply.github.com>
2022-01-08 19:13:52 +0900

    백준_2884번.py

---
+++ 백준_2884번.py
@@ -0,0 +1,9 @@
+a, b = map(int, input().split()) # 값 대입
+if b - 45 < 0 : # 조건문 if, else
+    b += 15 # b = b + 15
+    a -= 1 # a = a - 1
+    if a < 0 :
+        a = 23
+else :
+    b -=45
+print(a, b) # 출력
