47971b7fa7ae68946bec4d64a2d96d1c95e7e5d1
zmtmqhdl <91964419+zmtmqhdl@users.noreply.github.com>
2022-01-08 19:14:05 +0900

    백준_9498번.py

---
+++ 백준_9498번.py
@@ -0,0 +1,11 @@
+score = int(input()) # 값 대입
+if score >= 90 : # 조건문 if, elif, else
+    print("A") # 출력
+elif score >= 80 : 
+    print("B")
+elif score >= 70 :
+    print("C")
+elif score >= 60 :
+    print("D")
+else :
+    print("F")
