d1a5506862be209ac33f2b8ce797053755f48a91
zmtmqhdl <91964419+zmtmqhdl@users.noreply.github.com>
2022-02-05 18:41:01 +0900

    백준_4673번.py

--- 백준_4673번.py
+++ 백준_4673번.py
@@ -1,5 +1,5 @@
 def d(n) : # 함수 생성
-    n = n + sum(list(map(int, str(n)))) # list(map(int, str(n))) -> n의 값을 str형으로 바꾸어 한 글자씩 int형으로 다시 바꿔 list에 넣음
+    n = n + sum(list(map(int, str(n)))) # list(map(int, str(n))) -> n의 값을 str형으로 바꾸어 한 문자씩 int형으로 다시 바꿔 list에 넣음
     return n # 값 반환
 NoSelfNumber = set() # NoSelfNumber라는 집합 생성
 for i in range(1, 10001) : # 반복문 for
