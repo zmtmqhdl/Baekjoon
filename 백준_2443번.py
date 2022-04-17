4d4ee36af3d9fdde7aecc594ff229edeab90bfa3
zmtmqhdl <91964419+zmtmqhdl@users.noreply.github.com>
2022-04-17 11:45:56 +0900

    Delete 백준_2443번.py

--- 백준_2443번.py
+++
@@ -1,6 +0,0 @@
-a = int(input()) # 값 대입
-b = a # b라는 변수에 a대입
-for i in range(1, a + 1) : # 반복문 for -> 1부터 a까지 i에 차례대로 대입하며 반복
-    print(' ' * (b - i) + '*' * (2 * i - 1)) # 출력
-for j in range(1, a) : # 반복문 for -> 1부터 a - 1까지 i에 차례대로 대입하며 반복
-    print(' ' * j + '*' * (2 * (b - j) - 1)) # 출력
